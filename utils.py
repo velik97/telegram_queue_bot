2cf876d6ebe581b69a78e2613445f2bdd6126f07
Nikita Velikovskiy <velik.nikita@gmail.com>
2017-12-19 03:20:20 +0300

    -server mistakes fixed

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from telebot import types
 
 def not_in_queue_markup():
+	
 	markup = types.ReplyKeyboardMarkup()
 	markup.row('Начать очередь')
 	markup.row('Присоединиться к очереди')
@@ -8,17 +9,19 @@ def not_in_queue_markup():
 	return markup
 
 def chose_queue_name_markup():
+	
 	markup = types.ReplyKeyboardMarkup()
 	markup.row('1', '2', '3')
-    markup.row('4','5','6')
+	markup.row('4','5','6')
 	markup.row('7','8','9')
 	markup.row('0')
 
 	return markup
 
 def finish_queue_markup():
+
 	markup = types.ReplyKeyboardMarkup()
-        markup.row('Закончить')
+	markup.row('Закончить')
 
 	return markup
 
