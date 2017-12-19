10df115092aec6089e0dcd8a38ee83f263c980ef
velik97 <velik.nikita@gmail.com>
2017-12-19 03:11:30 +0300

    First server commit

--- bot.py
+++ bot.py
@@ -1,13 +1,45 @@
-import config
 import telebot
+import cherrypy
+
+import config
 import dialogs
+import utils
 
 from data_base_executer import DataBaseExecuter, Queue, QueueUser
 from datetime import datetime, timedelta
 
+WEBHOOK_HOST = '78.155.206.129'
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (config.token)
+
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(commands=['start_queue'])
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+
+	bot.send_message(message.chat.id, 'Приветик', reply_markup=utils.not_in_queue_markup())
+
+@bot.message_handler(regexp=['Начать очередь'])
 def start_queue(message):
 
 	with DataBaseExecuter(config.db_host) as db:
@@ -24,28 +56,37 @@ def start_queue(message):
 
 		db.update_user(creator)
 
-		bot.send_message(message.chat.id, dialogs.queue_started_respond(str(queue.name)))
-
-
-@bot.message_handler(commands=['join_queue'])
-def join_queue(message):
-
-	if (len(message.text.split(" ")) != 2):
-		bot.send_message(message.chat.id, dialogs.queue_start_bad_format_respond)
+		bot.send_message(message.chat.id, dialogs.queue_started_respond(str(queue.name)), reply_markup = utils.finish_queue_markup())
 		return
+
+
+@bot.message_handler(regexp=['Присоединиться к очереди'])
+def pre_join_queue(message):
 
 	with DataBaseExecuter(config.db_host) as db:
 
-		if (db.user_is_in_queue(message.chat.id)):
-			bot.send_message(message.chat.id, dialogs.already_in_queue_respond)
-			return
+                if (db.user_is_in_queue(message.chat.id)):
+                        bot.send_message(message.chat.id, dialogs.already_in_queue_respond)
+                        return
 
-		queue_name = message.text.split(" ")[1]
+		#pass
+		#bot.send_message(message.chat.id, 'Укажите номер очереди')
+		#, reply_markup = utils.chose_queue_name_markup())
+
+		#bot.register_next_step_handler(msg, join_queue)
+
+
+def join_queue(message):
+
+	with DataBaseExecuter(config.db_host) as db:
+
+		queue_name = message.text
 
 		queue = db.find_active_queue_by_name(queue_name)
 
 		if queue == None:
-			bot.send_message(message.chat.id, dialogs.no_such_queue_respond)
+			msg = bot.send_message(message.chat.id, dialogs.no_such_queue_respond)
+			bot.register_next_step_handler(msg, join_queue)
 			return
 
 		new_user = db.create_new_user(message.chat.id, queue.id)
@@ -62,10 +103,10 @@ def join_queue(message):
 		db.update_user(new_user)
 		db.update_queue(queue)
 
-		bot.send_message(message.chat.id, dialogs.join_success_respond(queue.name, str(new_user.num)))
+		bot.send_message(message.chat.id, dialogs.join_success_respond(queue.name, str(new_user.num)), reply_markup = utils.clear_prev_markup())
 
 
-@bot.message_handler(commands=['finish'])
+@bot.message_handler(regexp=['Закончить'])
 def finish_queue(message):
 	
 	with DataBaseExecuter(config.db_host) as db:
@@ -79,7 +120,7 @@ def finish_queue(message):
 		queue = db.find_queue(user.queue_id)
 
 		if queue.current_user_id != user.id:
-			bot.send_message(message.chat.id, dialogs.not_your_turn_respond)
+			msg = bot.send_message(message.chat.id, dialogs.not_your_turn_respond)
 			return
 
 		if user.id == queue.last_user_id:
@@ -92,7 +133,7 @@ def finish_queue(message):
 			queue.current_user_id = next_user.id
 
 			db.update_user(next_user)
-			bot.send_message(next_user.chat_id, dialogs.call)
+			bot.send_message(next_user.chat_id, dialogs.call, reply_markup = utils.finish_queue_markup())
 
 		user.is_in_queue = False
 		user.is_in_session = False
@@ -102,8 +143,20 @@ def finish_queue(message):
 
 		db.update_user(user)
 		db.update_queue(queue)
-		bot.send_message(message.chat.id, dialogs.goodbye_respond)
-		
+		bot.send_message(message.chat.id, dialogs.goodbye_respond, reply_markup = utils.not_in_queue_markup())
 
-if __name__ == '__main__':
-	bot.polling(none_stop=True)
+
+bot.remove_webhook()
+
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+cherrypy.config.update({
+    'server.socket_host': WEBHOOK_LISTEN,
+    'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
+})
+
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
---
+++ utils.py
@@ -0,0 +1,26 @@
+from telebot import types
+
+def not_in_queue_markup():
+	markup = types.ReplyKeyboardMarkup()
+	markup.row('Начать очередь')
+	markup.row('Присоединиться к очереди')
+
+	return markup
+
+def chose_queue_name_markup():
+	markup = types.ReplyKeyboardMarkup()
+	markup.row('1', '2', '3')
+        markup.row('4','5','6')
+	markup.row('7','8','9')
+	markup.row('0')
+
+	return markup
+
+def finish_queue_markup():
+	markup = types.ReplyKeyboardMarkup()
+        markup.row('Закончить')
+
+	return markup
+
+def clear_prev_markup():
+	return types.ReplyKeyboardHide()
