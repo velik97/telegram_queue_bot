315f50d593afa08ed82484cfc84c0e0bf983ce08
Nikita Velikovskiy <velik.nikita@gmail.com>
2017-12-19 03:47:36 +0300

    -fix

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 
 WEBHOOK_HOST = '78.155.206.129'
 WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+WEBHOOK_LISTEN = '78.155.206.129'  # На некоторых серверах придется указывать такой же IP, что и выше
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
